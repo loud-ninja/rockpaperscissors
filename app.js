adab7b7c6678d4c6acd04121d701cf16bf4f024e
Willy Wu <www.www.com@gmail.com>
2020-05-28 12:32:02 +0800

    debugged app.js and created logic for rockpaperscissors. Need to debug if empty entry. Need to allow for case insensitive input

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const gameOptions = ['rock','paper','scissors'];
+//let str = prompt('rock, paper, scissors?')
 let playerSelection = prompt('rock, paper, scissors?');
+//str.match(/geeks/gi); 
 
 function computerPlay() {
     // 
     let choice = Math.floor(Math.random() * gameOptions.length)
-    console.log(gameOptions[choice]);
+    return gameOptions[choice];
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    let computerSelection = computerPlay();
     if (playerSelection == 'rock' ) {
         if (computerSelection == 'rock') {
-        alert(`Draw. ${computerSelection}`) 
+            alert(`Draw. ${computerSelection}`) 
         } else if (computerSelection == 'scissors') {
-        alert(`Player Wins. ${playerSelection} wins ${computerSelection}`);
+            alert(`Player Wins. ${playerSelection} wins ${computerSelection}`);
         } else if (computerSelection == 'paper') {
-        alert(`Computer Wins. ${computerSelection} wins ${playerSelection}`);
-        }
-    } 
+            alert(`Computer Wins. ${computerSelection} wins ${playerSelection}`);
+        } else {alert ('Please choose Rock, Paper, Scissors.')}
+
+    } if (playerSelection == 'paper' ) {
+        if (computerSelection == 'paper') {
+            alert(`Draw. ${computerSelection}`)
+        } else if (computerSelection == 'rock') {
+            alert(`Player Wins. ${playerSelection} wins ${computerSelection}`);
+        } else if (computerSelection == 'scissors') {
+            alert(`Computer Wins. ${computerSelection} wins ${playerSelection}`)
+        } else {alert ('Please choose Rock, Paper, Scissors.')}
+
+    } if (playerSelection == 'scissors') {
+        if (computerSelection == 'scissors') {
+            alert(`Draw. ${computerSelection}`)
+        } else if (computerSelection == 'paper'){
+            alert(`Player Wins. ${playerSelection} wins ${computerSelection}`);
+        } else if (computerSelection == 'rock') {
+            alert(`Computer Wins. ${computerSelection} wins ${playerSelection}`)
+        } else {alert ('Please choose Rock, Paper, Scissors.')}
+    } else {alert ('Please choose Rock, Paper, Scissors.')}
 }
-/* */
 
-// const computerSelection = computerPlay()
 
-// console.log(playRound(playerSelection, computerSelection))
+const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
+
+console.log(playRound(playerSelection, computerSelection))
 
 
 
